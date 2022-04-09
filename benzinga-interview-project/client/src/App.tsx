87e5df25208536c9fb17b940b6377e4a397c3407
Ryan Lesson <rlesson@viatechpub.com>
2022-04-09 16:46:45 -0500

    get everything rolling in client applicaiton

---
+++ benzinga-interview-project/client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { } from '@benzinga/data'
+
+function App() {
+  return (
+    <div className="App">
+      <div>hello</div>
+    </div>
+  );
+}
+
+export default App;
